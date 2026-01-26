52785d9181956681fdd8cb6e044e9d1f37ef5bc2
Jonas <jonas.kaun@crpusd.org>
2026-01-26 10:01:13 -0800

    First commit with assignment template

---
+++ TraversalAlgorithmsFor.java
@@ -0,0 +1,34 @@
+/*
+ * Activity 3.1.2 
+ */
+public class TraversalAlgorithmsFor
+{
+  public static void main(String[] args)
+  {
+    algorithm1();
+    algorithm2();
+    algorithm3();
+    algorithm4();
+  }
+  
+  public static void algorithm1()
+  {
+    /* your code here */
+  }
+  
+  public static void algorithm2()
+  {
+    /* your code here */
+  }
+  
+   public static void algorithm3()
+  {
+    /* your code here */
+  }
+  
+  public static void algorithm4()
+  {
+    /* your code here */
+  }
+  
+}
