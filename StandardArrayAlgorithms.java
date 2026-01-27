d16b9e66689eb367e591c848da69f33ed6736af2
Jonas <jonas.kaun@crpusd.org>
2026-01-27 09:45:49 -0800

    added max value finder

--- StandardArrayAlgorithms.java
+++ StandardArrayAlgorithms.java
@@ -9,9 +9,25 @@ public class StandardArrayAlgorithms
     
     int sum = 0;
     for (int i = 0; i < goals.length; i++)
+      //record each goal to a single sum int
       sum += goals[i];
-    
     System.out.println("All goals: " + sum);
+    // print average goals per game, no rounding
+    double averageGoals = (sum)/((1.0)*(goals.length)); 
+    System.out.println("Average goals: "+ averageGoals);
+
+
+    // finding the max value
+    int max = 0;
+    for(int goal: goals){
+      if (goal>max){
+        max = goal;
+      }
+    }
+    System.out.println("The max goal amount was: "+max);
   }
+
+  
+
   
 }
